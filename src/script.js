9a986b7122fe1c26019d7e2298393cdea1e1e6dc
Kai Fukushima <k_fukushima@yumemi.co.jp>
2022-09-12 16:29:47 +0900

    update: オリジナルスクラムゲームにアップデート

--- src/script.js
+++ src/script.js
@@ -1,10 +1,10 @@
 import {sleep, $, fade, num2str, rand} from "./functions";
 
 // 変数定義
-let timer;
 let isTwice = false;
 let isAnimating = false;
 let prevCard;
+const numPairs = 18;
 const areas = "a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J".split(" ");
 const icons = `<i class="far fa-heart"></i>
 <i class="far fa-gem"></i>
@@ -26,7 +26,6 @@ const icons = `<i class="far fa-heart"></i>
 <i class="far fa-star"></i>`.split(`\n`);
 
 const allCards = icons.concat(icons);
-
 let cardRemaining = icons.length;
 
 const check = async function(){
@@ -55,57 +54,82 @@ const check = async function(){
     }
 }
 
+const plan_phase = async () => {
+    const limitSec = 120;
+
+    let sec = 0;
+    while (sec < limitSec) {
+        await sleep(1000);
+        sec++;
+        $('span#plan-time').innerText = `残り${num2str(limitSec - sec)}秒`;
+    }
+    return;
+}
+
+const game_phase = async () => {
+    const limitSec = 120;
+
+    $('span#remaining').innerText = cardRemaining;
+
+    for(let i=1; i<37; i++){
+        const randIndex = rand(allCards.length);
+        const card = document.createElement('div');
+        card.innerHTML = allCards[randIndex];
+        card.classList.add("card",'clicked');
+        card.style.gridArea = areas[i-1];
+        $('.boardContainer').appendChild(card);
+        allCards.splice(randIndex, 1);
+    }
+    await sleep(1600);
+    
+    $('span#game-time').innerText = `10秒間で配置を覚えてください（ドライバーは目を閉じてください）。`;
+    await sleep(7000);
+    $('span#game-time').innerText = `3`;
+    await sleep(1000);
+    $('span#game-time').innerText = `2`;
+    await sleep(1000);
+    $('span#game-time').innerText = `1`;
+    await sleep(1000);
+    $('span#game-time').innerText = `スタート！`;
+
+    document.querySelectorAll('.card').forEach(_=>{
+        _.classList.remove('clicked');
+        _.addEventListener('click',check);
+    });
+
+    let sec = 0;
+    while (sec < limitSec && cardRemaining > 0) {
+        await sleep(1000);
+        sec++;
+        $('span#game-time').innerText = `残り${num2str(limitSec-sec)}秒`;
+    }
+    $('span#finTime').innerText = `${num2str(sec)}秒`;
+    $('span#finPair').innerText = `${numPairs - cardRemaining}ペア`;
+    return;
+}
+
+const review_phase = async () => {
+    const limitSec = 60;
+
+    let sec = 0;
+    while (sec < limitSec) {
+        await sleep(1000);
+        sec++;
+        $('span#review-time').innerText = `残り${num2str(limitSec - sec)}秒`;
+    }
+    return;
+}
+
 // Main
 window.addEventListener('load',()=>{
     $("#start").addEventListener('click', async ()=>{
-        $('span#remaining').innerText = cardRemaining;
-        fade($('.start'),$('.game'));
-        for(let i=1; i<37; i++){
-            const randIndex = rand(allCards.length);
-            const card = document.createElement('div');
-            card.innerHTML = allCards[randIndex];
-            card.classList.add("card",'clicked');
-            card.style.gridArea = areas[i-1];
-            $('.boardContainer').appendChild(card);
-            allCards.splice(randIndex, 1);
-        }
-        await sleep(1600);
-        let min = 0;
-        let sec = 0;
-        let milSec = 0;
-        $('span#time').innerText = `Memorize these cards within 10 secs.`;
-        await sleep(7000);
-        $('span#time').innerText = `3`;
-        await sleep(1000);
-        $('span#time').innerText = `2`;
-        await sleep(1000);
-        $('span#time').innerText = `1`;
-        await sleep(1000);
-        $('span#time').innerText = `Start`;
-        document.querySelectorAll('.card').forEach(_=>{
-            _.classList.remove('clicked');
-            _.addEventListener('click',check);
-        });
-        await sleep(1000);
-        timer = setInterval(async ()=>{
-            milSec++;
-            if(milSec >= 10){
-                sec++;
-                milSec -= 10;
-            }
-            if(sec >= 60){
-                min++;
-                sec -= 60;
-            }
-            $('span#time').innerText = `${num2str(min)} : ${num2str(sec)} . ${num2str(milSec,1)}`;
-
-            if(cardRemaining <= 0){
-                clearInterval(timer);
-                fade($('.game'),$('.end'));
-                $('span#finTime').innerText = `${num2str(min)} : ${num2str(sec)} . ${num2str(milSec,1)}`;
-
-            }
-        },100)
+        fade($('.start'),$('.plan-phase'));
+        await plan_phase();
+        fade($('.plan-phase'),$('.game-phase'));
+        await game_phase();
+        fade($('.game-phase'),$('.review-phase'));
+        await review_phase();
+        fade($('.review-phase'),$('.end'));
     });
     $('#again').addEventListener('click',()=>{
         location.reload();
